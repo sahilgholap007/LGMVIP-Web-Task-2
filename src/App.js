6b8ff395ea69d7983262e64ffdfeccc2e451ca8e
sahil gholap <gholapsahil007@gmail.com>
2023-07-02 15:01:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import React from "react";
 import Card from "./Components/Card";
 import { useState } from "react";
 import axios from "axios";
+import RingLoader from "react-spinners/RingLoader";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const handleGetUsers = async() => {
+  const handleGetUsers = async () => {
     await axios.get("https://reqres.in/api/users?page=1").then((res) => {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1200);
       setUsers(res.data.data);
     });
   };
@@ -33,10 +39,20 @@ function App() {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
           alt="logo"
         />
-        <button className="btn" onClick={handleGetUsers}>GET USERS</button>
+        <button className="btn" onClick={handleGetUsers}>
+          GET USERS
+        </button>
       </nav>
       <div className="cards-container">
-        {card}
+        {loading ? (
+          <RingLoader	
+            color="#61DBFB"
+            size={100}
+            className="loader"
+          />
+        ) : (
+          card
+        )}
       </div>
     </div>
   );
